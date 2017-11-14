56523d215c8295b9091a733b37483eb0d5e6bbaf
kaojo <mail@kaojo.de>
2017-11-14 21:18:54 +0100

    added generated squares flag

--- sudojo_sudoku_core/src/sudoku/game/square.rs
+++ sudojo_sudoku_core/src/sudoku/game/square.rs
@@ -4,6 +4,7 @@ pub struct Square {
     pub value: u8,
     pub initial: bool,
     pub conflict: bool,
+    pub generated: bool,
 }
 
 impl Square {
@@ -12,6 +13,15 @@ impl Square {
             value: value,
             initial: initial,
             conflict: false,
+            generated: false,
+        }
+    }
+    pub fn generate(value: u8, initial: bool) -> Self {
+        Square {
+            value: value,
+            initial: initial,
+            conflict: false,
+            generated: true,
         }
     }
 }
